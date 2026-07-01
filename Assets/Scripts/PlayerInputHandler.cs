7f7b8b6c898e4d9539455398cebaab0f514f060d
AniruddhSin <aniruddhsindhu@gmail.com>
2026-06-30 22:30:10 -0700

    fixed pause button bug

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -79,5 +79,9 @@ public class PlayerInputHandler : MonoBehaviour
             pauseMenu.TogglePause();
         }
     }
+    public void enableInput()
+    {
+        inputAllowed = true;
+    }
 
 }
